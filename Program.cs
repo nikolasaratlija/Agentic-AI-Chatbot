1af7aa56ad70decdeb49315b26fddc1dcdad08b5
Nikola Saratlija <nikola@Nikolas-MacBook-Air.local>
2026-07-14 09:19:31 +0200

    added aspire

--- Program.cs
+++ Program.cs
@@ -6,6 +6,10 @@ using Microsoft.Agents.AI.Workflows;
 using OpenAI;
 using Models;
 using System.ComponentModel;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Resources;
 
 #region init
 var serviceUrl = Environment.GetEnvironmentVariable("FOUNDRY_SERVICE_URL") 
@@ -14,6 +18,14 @@ var serviceUrl = Environment.GetEnvironmentVariable("FOUNDRY_SERVICE_URL")
 var apiKey = Environment.GetEnvironmentVariable("AZURE_OPENAI_TOKEN") 
     ?? throw new InvalidOperationException("Environment variable 'AZURE_OPENAI_TOKEN' is missing.");
 var model = "gpt-5-mini";
+
+ILoggerFactory loggerFactory = LoggerFactory.Create(
+    builder =>
+    {
+        builder.AddConsole();
+        builder.SetMinimumLevel(LogLevel.Trace);
+    }
+);
 
 OpenAIClient client = new(
     new ApiKeyCredential(apiKey),
@@ -47,53 +59,48 @@ DatabaseToolsService databaseTools = new("data/ehr.db");
 
 var ehrAgent = new ChatClientAgent(
     chatClient,
+    loggerFactory: loggerFactory,
     instructions: EHRAgentPrompt,
     name: "EHR Agent",
     tools: [
         AIFunctionFactory.Create(databaseTools.GetDatabaseSchema),
         AIFunctionFactory.Create(databaseTools.GetTableSchema),
         AIFunctionFactory.Create(databaseTools.ExecuteQuery),
-    ]);
+    ]).AsBuilder().UseOpenTelemetry("chatbot-prototype-ehr").Build();
 #endregion
 
 #region orchestrator agent
-#region workflow
-// var vizAgentExecutor = new VizAgentExecutor(vizAgent);
-// var persistJSONExecutor = new PersistJSONExecutor<VegaLiteSpec>("file.json");
-
-// var vizToolOptions = new AIFunctionFactoryOptions()
-// {
-//     Name = "Visualisation-Tool",
-//     Description = "Generates a visualization from a user's natural-language request. Result is persisted to disk."
-// };
-
-// var visualizationWorkflow = new WorkflowBuilder(vizAgentExecutor)
-//     .AddEdge(vizAgentExecutor, persistJSONExecutor)
-//     .Build()
-//     .AsAIAgent(
-//         id: "visualization-generator",
-//         name: "visualization-generator",
-//         description: "Generates a visualization from a user's natural-language request. Result is persisted to disk.")
-//     .AsAIFunction(vizToolOptions);
-#endregion
-
 DataVizTool dataVizTool = new(vizAgent);
 string orchAgentPrompt = File.ReadAllText("agent_prompts/OrchestratorInstructions.txt");
 
 var orchestratorAgent = new ChatClientAgent(
     chatClient,
-    name: "Orchestrator Agent",
+    loggerFactory: loggerFactory,
     instructions: orchAgentPrompt,
+    name: "Orchestrator Agent",
     tools: [
         ehrAgent.AsAIFunction(),
         AIFunctionFactory.Create(dataVizTool.GenerateVisualisation),
-        // visualizationWorkflow
-    ]);
+    ]).AsBuilder().UseOpenTelemetry("chatbot-prototype-orchestrator").Build();;
 #endregion
 
 AgentSession session = await orchestratorAgent.CreateSessionAsync();
 
 Console.WriteLine("EHR AI Chatbot");
+
+var resourceBuilder = ResourceBuilder
+    .CreateDefault()
+    .AddService("myapplication");
+
+using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+    .SetResourceBuilder(resourceBuilder)
+    .AddSource("chatbot-prototype-orchestrator")
+    .AddSource("chatbot-prototype-ehr")
+    .AddOtlpExporter(options => options.Endpoint = new Uri("http://localhost:4317"))
+    .Build();
+
+// var response = await orchestratorAgent.RunAsync("what is the distribution of sex between patients");
+// Console.WriteLine(response.Messages);
 
 // Create a bar chart of the amount of male and female patients.
 // Create a barchart: 50 males, 60 females
